d4c205779e944e9367d1f2348fd9421cadf88168
maske771 <111384096+maske771@users.noreply.github.com>
2026-09-15 20:29:40 +0300

    fix(alertmanager): avoid multiline sed continuation

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-
-sed \
-  -e "s|\${ALERTMANAGER_SMTP_SMARTHOST}|${ALERTMANAGER_SMTP_SMARTHOST:-smtp.example.com:587}|g" \
-  -e "s|\${ALERTMANAGER_SMTP_FROM}|${ALERTMANAGER_SMTP_FROM:-opspilot@example.com}|g" \
-  -e "s|\${ALERTMANAGER_SMTP_USERNAME}|${ALERTMANAGER_SMTP_USERNAME:-opspilot@example.com}|g" \
-  -e "s|\${ALERTMANAGER_SMTP_PASSWORD}|${ALERTMANAGER_SMTP_PASSWORD:-change-me}|g" \
-  -e "s|\${ALERTMANAGER_EMAIL_TO}|${ALERTMANAGER_EMAIL_TO:-ops@example.com}|g" \
-  -e "s|\${ALERTMANAGER_TELEGRAM_BOT_TOKEN}|${ALERTMANAGER_TELEGRAM_BOT_TOKEN:-replace-me}|g" \
-  -e "s|\${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|g" \
-  /etc/alertmanager/alertmanager.yml.tpl > /tmp/alertmanager.yml
-
+sed -e "s|\${ALERTMANAGER_SMTP_SMARTHOST}|${ALERTMANAGER_SMTP_SMARTHOST:-smtp.example.com:587}|g" -e "s|\${ALERTMANAGER_SMTP_FROM}|${ALERTMANAGER_SMTP_FROM:-opspilot@example.com}|g" -e "s|\${ALERTMANAGER_SMTP_USERNAME}|${ALERTMANAGER_SMTP_USERNAME:-opspilot@example.com}|g" -e "s|\${ALERTMANAGER_SMTP_PASSWORD}|${ALERTMANAGER_SMTP_PASSWORD:-change-me}|g" -e "s|\${ALERTMANAGER_EMAIL_TO}|${ALERTMANAGER_EMAIL_TO:-ops@example.com}|g" -e "s|\${ALERTMANAGER_TELEGRAM_BOT_TOKEN}|${ALERTMANAGER_TELEGRAM_BOT_TOKEN:-replace-me}|g" -e "s|\${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|g" /etc/alertmanager/alertmanager.yml.tpl > /tmp/alertmanager.yml
 exec /bin/alertmanager --config.file=/tmp/alertmanager.yml --storage.path=/alertmanager
