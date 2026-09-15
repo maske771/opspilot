914e9ecca7c88b897ea800a7e10e3cc3d18733ce
maske771 <111384096+maske771@users.noreply.github.com>
2026-09-15 18:08:18 +0300

    fix(monitoring): make Alertmanager entrypoint shell-compatible

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -eu
 
 sed \
   -e "s|\${ALERTMANAGER_SMTP_SMARTHOST}|${ALERTMANAGER_SMTP_SMARTHOST}|g" \
@@ -8,7 +7,7 @@ sed \
   -e "s|\${ALERTMANAGER_SMTP_PASSWORD}|${ALERTMANAGER_SMTP_PASSWORD}|g" \
   -e "s|\${ALERTMANAGER_EMAIL_TO}|${ALERTMANAGER_EMAIL_TO}|g" \
   -e "s|\${ALERTMANAGER_TELEGRAM_BOT_TOKEN}|${ALERTMANAGER_TELEGRAM_BOT_TOKEN}|g" \
-  -e "s|\${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|${ALERTMANAGER_TELEGRAM_CHAT_ID}|g" \
+  -e "s|\${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|g" \
   /etc/alertmanager/alertmanager.yml.tpl > /tmp/alertmanager.yml
 
 exec /bin/alertmanager --config.file=/tmp/alertmanager.yml --storage.path=/alertmanager
