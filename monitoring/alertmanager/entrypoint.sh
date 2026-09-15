d6c9216b5a8ea7a56a935682dd10ebeb0df80e17
maske771 <111384096+maske771@users.noreply.github.com>
2026-09-15 18:06:25 +0300

    fix(alertmanager): render environment variables before startup

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -eu
 
-cat > /tmp/alertmanager.yml <<EOF
-$(cat /etc/alertmanager/alertmanager.yml.tpl)
-EOF
+sed \
+  -e "s|\${ALERTMANAGER_SMTP_SMARTHOST}|${ALERTMANAGER_SMTP_SMARTHOST}|g" \
+  -e "s|\${ALERTMANAGER_SMTP_FROM}|${ALERTMANAGER_SMTP_FROM}|g" \
+  -e "s|\${ALERTMANAGER_SMTP_USERNAME}|${ALERTMANAGER_SMTP_USERNAME}|g" \
+  -e "s|\${ALERTMANAGER_SMTP_PASSWORD}|${ALERTMANAGER_SMTP_PASSWORD}|g" \
+  -e "s|\${ALERTMANAGER_EMAIL_TO}|${ALERTMANAGER_EMAIL_TO}|g" \
+  -e "s|\${ALERTMANAGER_TELEGRAM_BOT_TOKEN}|${ALERTMANAGER_TELEGRAM_BOT_TOKEN}|g" \
+  -e "s|\${ALERTMANAGER_TELEGRAM_CHAT_ID:-0}|${ALERTMANAGER_TELEGRAM_CHAT_ID}|g" \
+  /etc/alertmanager/alertmanager.yml.tpl > /tmp/alertmanager.yml
 
 exec /bin/alertmanager --config.file=/tmp/alertmanager.yml --storage.path=/alertmanager
